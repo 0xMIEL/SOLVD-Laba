ded836a1e07a1129d720a2f6bc7e18fb76dc9eb3
CMielczarek <mielczarekcasper@gmail.com>
2024-06-08 22:55:46 +0200

    first commit 08.06

--- Notes/2. How do the modules work?/ES-modules/module.mjs
+++
@@ -1,8 +0,0 @@
-// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/export
-
-export  function add(x, y) {
-    return x + y
-}
-export function subtract(x, y) {
-    return x - y
-}
